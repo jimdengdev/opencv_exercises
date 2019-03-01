7390e3745035c990f959c0cb177ae8ebc7538578
Betterming <35894046+Betterming@users.noreply.github.com>
2019-03-01 21:57:36 +0800

    second

--- tutorial_16_grad.py
+++ tutorial_16_grad.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # 图像梯度（由x,y方向上的偏导数和偏移构成），有一阶导数（sobel算子）和二阶导数（Laplace算子）
 # 用于求解图像边缘，一阶的极大值，二阶的零点
-# 一阶偏导在图像中为一阶差分，再变成算子（即权值）与图像像素值乘积相加，而且同理
+# 一阶偏导在图像中为一阶差分，再变成算子（即权值）与图像像素值乘积相加，二阶同理
 def sobel_demo(image):
     grad_x = cv.Sobel(image, cv.CV_32F, 1, 0)  # 采用Scharr边缘更突出
     grad_y = cv.Sobel(image, cv.CV_32F, 0, 1)
